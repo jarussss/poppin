9960879e534a101d50959d38932e415ee2a8064d
jarussss <125578729+jarussss@users.noreply.github.com>
2023-02-16 16:21:26 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+#dudnią mi plemniki w jajach
+print("dududupson")
+
+x="dududupson"
+zmienna="cos"
+print(len(x))#liczy znakii je wypluwa
+print(x[0])#pisze znak ze stringa w pozycji 0 czyli s
+print(x[1:5])#to samo ale od 1 do 5
+print(x[2:])# liczy od dwa do końca
+print(x[:3])# liczy do 3
+print(x[:])#maksymalna wartość od jednej strony do drugiej(nie potrzebne
+print(x[::2])#liczy co drugi
+
+imie = "Jaromir"
+nazwisko = "limanówka"
+
+print(imie + " " + nazwisko)
+
+print(imie, nazwisko, end="---")
+print(imie, nazwisko, end="---")
+print(imie, nazwisko, end="---")
+print(imie, nazwisko, )
+#I need my drugs, I need my love (we get high)
+#Bitches pullin' up and they suck us up (hey)
+#I'm in the Tonka, baby, yeah, come show me love (ooh)
+#Io#n't fuck with nobody, lil' bitch, I show you nun' (show you nun')
+#I ain't trustin' nobody, lil' bitch, I keep a gun
+#If a problem come up (yeah), you know I'm not gon' run
+#Feel like Osama to us, 'cause we blow up like a bomb (boom)
+#Roll him like a filter, yeah, then smoke him like a bong (phew)
+
+print(f"witaj {imie} tajskie żarcie?")
+
+x = input("podaj swoj wiek")
+print(int(x) + 2)
+print("wiek to tylko liczba")
+james_bond = 7
+print(str(james_bond).zfill(3))
